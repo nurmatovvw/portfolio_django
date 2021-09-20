48c1c0589e60ed546d2857ba671ae88f09767aba
nurmatovvw <abdulkhakimn@mail.ru>
2021-09-20 14:43:31 +0600

    added admin-pponel for site

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from django.db import models
+from .models import SendMessage
 # Register your models here.
+
+
+class SendMessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'phone_number', 'message', 'sent_at',)
+    search_fields = ('email',)
+    list_editable = ('message',)
+    list_filter = ('sent_at',)
+    ordering = ('pk','sent_at',)
+
+admin.site.register(SendMessage, SendMessageAdmin)
